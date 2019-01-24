3b86ec4a203d3c9369c8223d6c5c21948a9253ac
Aishwarya0811 <41635755+Aishwarya0811@users.noreply.github.com>
2019-01-23 23:09:42 -0600

    Update Shape.py

--- Shape/Shape.py
+++ Shape/Shape.py
@@ -1,6 +1,4 @@
 
-
-
-import numpy
-my_array = numpy.array([1,2,3,4,5,6,7,8,9])
-print numpy.reshape(my_array,(3,3))
+import numpy as np
+array = np.array(list(map(int, input().split())))
+print(array.reshape(3, 3))
