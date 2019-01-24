cfaae653d13632d08bdc377b529300ad5161f955
Aishwarya0811 <41635755+Aishwarya0811@users.noreply.github.com>
2019-01-23 23:16:35 -0600

    Update Shape.py

--- Shape/Shape.py
+++ Shape/Shape.py
@@ -1,4 +1,10 @@
 
+## The input to the script is separated by space. 
+##The split function will use space as a delimiter and get all the inputs separately as a list.
+##Then you end up with characters which you must convert to integers. 
+##You do that by mapping the function int.
+
+Then you just convert it to a list.
 import numpy as np
 array = np.array(list(map(int, input().split())))
 print(array.reshape(3, 3))
