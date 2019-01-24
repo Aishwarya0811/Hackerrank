c0a22621f753872ba80fb44b5921db53f88057dd
Aishwarya0811 <41635755+Aishwarya0811@users.noreply.github.com>
2019-01-23 23:25:54 -0600

    Update Shape.py

--- Shape/Shape.py
+++ Shape/Shape.py
@@ -3,7 +3,7 @@
 ##The split function will use space as a delimiter and get all the inputs separately as a list.
 ##Then you end up with characters which you must convert to integers. 
 ##You do that by mapping the function int.
-
+##Input Function lets you ask a user for some text input.
 Then you just convert it to a list.
 import numpy as np
 array = np.array(list(map(int, input().split())))
