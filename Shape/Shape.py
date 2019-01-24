5fe7e6b1dad5166dcc2686afa1141f94613fa4cc
Aishwarya0811 <41635755+Aishwarya0811@users.noreply.github.com>
2019-01-23 23:26:11 -0600

    Update Shape.py

--- Shape/Shape.py
+++ Shape/Shape.py
@@ -4,7 +4,7 @@
 ##Then you end up with characters which you must convert to integers. 
 ##You do that by mapping the function int.
 ##Input Function lets you ask a user for some text input.
-Then you just convert it to a list.
+##Then you just convert it to a list.
 import numpy as np
 array = np.array(list(map(int, input().split())))
 print(array.reshape(3, 3))
