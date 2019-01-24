8df9e0cdef5f8da9c008cfe40801fdf99882b11e
Aishwarya0811 <41635755+Aishwarya0811@users.noreply.github.com>
2019-01-23 23:30:22 -0600

    Update Shape.py

--- Shape/Shape.py
+++ Shape/Shape.py
@@ -5,6 +5,7 @@
 ##You do that by mapping the function int.
 ##Input Function lets you ask a user for some text input.
 ##Then you just convert it to a list.
+##map function makes an iterator that computes the function using arguments from each of the iterables
 import numpy as np
 array = np.array(list(map(int, input().split())))
 print(array.reshape(3, 3))
