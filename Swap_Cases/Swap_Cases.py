5de5b8b6a3f6392c2f209ea71433bba88b470035
AISHWARYA BADLANI <aishwarya08badlani@gmail.com>
2019-02-10 20:11:56 -0600

    Swap_Cases

---
+++ Swap_Cases/Swap_Cases.py
@@ -0,0 +1,15 @@
+def swap_case(s):   
+    return s.swapcase()
+
+
+
+
+
+    def swap_case(s):
+    a = ""
+    for let in s:
+        if let.isupper() == True:
+            a+=(let.lower())
+        else:
+            a+=(let.upper())
+    return a
