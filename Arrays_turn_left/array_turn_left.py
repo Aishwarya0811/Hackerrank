64b26bd619c0d23bf21f82615d93f9b7b6e02943
AISHWARYA BADLANI <aishwarya08badlani@gmail.com>
2019-07-02 01:38:39 -0400

    turn_left

---
+++ Arrays_turn_left/array_turn_left.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    temp = []
+    for i in range(len(a)):
+        temp.append(a[i])
+    for j in range(len(a)):
+        a[j-d]=temp[j]
+    return a
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
